468263e3c7e7fd1d246c976ccddd8833add23e7a
Teflon-256 <sahabyoona@gmail.com>
2025-09-22 05:46:51 +0300

    Create HeroHome.tsx

---
+++ client/src/components/ui/HeroHome.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HeroHome: React.FC = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: 'url("/attached-assets/generated-images/Futuristic_stock_exchange_wallpaper_8045bc0a.png")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '300px',
+        borderRadius: '12px',
+      }}
+    />
+  );
+};
+
+export default HeroHome;
