8cecdfcf4ecae1fad9b3121d255443ce64cfa8a7
Teflon-256 <sahabyoona@gmail.com>
2025-09-22 05:48:06 +0300

    Create Landing.tsx

---
+++ client/src/components/ui/Landing.tsx
@@ -0,0 +1,11 @@
+import HeroLanding from "@/components/ui/HeroLanding";
+
+export default function Landing() {
+  return (
+    <div>
+      <HeroLanding />
+      <h1>Welcome to AlvaPortal</h1>
+      {/* Other landing content */}
+    </div>
+  );
+}
